e347787e5deeb411bfb417205ebae844f62f7970
hkr691 <hkr691@gmail.com>
2026-08-18 17:21:20 -0600

    array and hashing day 1

---
+++ 1.Array_Hashing/1.ContainsDuplicates.py
@@ -0,0 +1,12 @@
+"""
+Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+Input: nums = [1, 2, 3, 3]
+
+Output: true
+
+"""
+
+class Solution:
+    def hasDups(self, array):
+        return len(set(array)) < len(array)
