bc6bb3fc0482bdadd7ee068bb2666a0d9f797e59
hkr691 <hkr691@gmail.com>
2026-08-19 21:27:05 -0600

    day2 - two pointers

--- 2.Two_pointers/2.ThreeSum.py
+++ 2.Two_pointers/2.ThreeSum.py
@@ -47,8 +47,8 @@ class Solution:
                         l += 1
                     while l < r and nums[r] == nums[r - 1]:
                         r -= 1
-                l += 1
-                r -= 1
+                    l += 1
+                    r -= 1
         return res
 
 sol = Solution()
