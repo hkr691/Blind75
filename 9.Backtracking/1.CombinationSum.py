f6151be6e9bf259484fdbd5fb000f2a7e92e01d1
hkr691 <hkr691@gmail.com>
2026-09-24 17:46:32 -0600

    backtracking

---
+++ 9.Backtracking/1.CombinationSum.py
@@ -0,0 +1,60 @@
+"""
+You are given an array of distinct integers nums and a target integer target. Your task is to return a list of all unique combinations of nums where the chosen numbers sum to target.
+
+The same number may be chosen from nums an unlimited number of times. Two combinations are the same if the frequency of each of the chosen numbers is the same, otherwise they are different.
+
+You may return the combinations in any order and the order of the numbers in each combination can be in any order.
+
+Example 1:
+
+Input:
+nums = [2,5,6,9]
+target = 9
+
+Output: [[2,2,5],[9]]
+
+Explanation:
+2 + 2 + 5 = 9. We use 2 twice, and 5 once.
+9 = 9. We use 9 once.
+
+Example 2:
+
+Input:
+nums = [3,4,5]
+target = 16
+
+Output: [[3,3,3,3,4],[3,3,5,5],[4,4,4,4],[3,4,4,5]]
+
+Example 3:
+
+Input:
+nums = [3]
+target = 5
+
+Output: []
+"""
+from typing import List
+
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        result = []
+        
+        def dfs(idx, curr, total):
+            if total == target:
+                result.append(curr.copy())
+                return
+            if idx >= len(nums) or total > target:
+                return
+            
+            curr.append(nums[idx])
+            dfs(idx, curr, total + nums[idx])
+            curr.pop()
+            dfs(idx + 1, curr, total)
+        
+        dfs(0, [], 0)
+        return result
+
+nums = [2,5,6,9]
+target = 9
+sol = Solution()
+print(sol.combinationSum(nums, target))
